86448506b2e186aca01337cd678f56d6ad4eb19f
Thomas Friberg <tomtheswede@gmail.com>
2016-04-24 14:59:54 +0800

    Removing button and fade related scope

--- ESP01-LED-Relay.ino
+++ ESP01-LED-Relay.ino
@@ -9,13 +9,12 @@
 #include <WiFiUdp.h>
 
 //Sensor details
-const char* sensorID1 = "LED003"; //Name of sensor
-const char* sensorID2 = "BUT003"; //Name of sensor
-const char* deviceDescription = "Outside light";
+const char* sensorID1 = "LED001"; //Name of sensor
+const char* deviceDescription = "Bedroom lamp";
 
 // WiFi parameters
-const char* ssid = "TheSubway";
-const char* password = "vanillamoon576";
+const char* ssid = ""; //Enter your WiFi network name here in the quotation marks
+const char* password = ""; //Enter your WiFi pasword here in the quotation marks
 
 //Server details
 unsigned int localPort = 5007;  //UDP send port
@@ -24,16 +23,12 @@ byte packetBuffer[512]; //buffer for incoming packets
 
 //Sensor variables
 const int ledPin = 2; //LED pin number
-const int butPin = 0; //Button pin
 int ledPinState = 0; //Default boot state of LEDs and last setPoint of the pin between 0 and 100
 int brightness = 100; //last 'on' setpoint for 0-100 scale brightness
 static const unsigned int PWMTable[101] = {0,1,2,3,5,6,7,8,9,10,12,13,14,16,18,20,21,24,26,28,31,33,36,39,42,45,49,52,56,60,64,68,72,77,82,87,92,98,103,109,115,121,128,135,142,149,156,164,172,180,188,197,206,215,225,235,245,255,266,276,288,299,311,323,336,348,361,375,388,402,417,432,447,462,478,494,510,527,544,562,580,598,617,636,655,675,696,716,737,759,781,803,826,849,872,896,921,946,971,997,1023}; //0 to 100 values for brightnes
 String data = "";
 String message = "";
 String devID = "";
-const int butLoopTime = 100; //millis between loops
-int lastButState = 0;
-int butState = 0;
 int butPushTime = 0; //millisecond timer for when the button is triggered
 int ledSetPoint = 0;
 int fadeSpeed = 18; //Time between fade intervals - 20ms between change in brightness
@@ -47,11 +42,11 @@ void setup()
 {
   //Set pins and turn off LED
   pinMode(ledPin, OUTPUT); //Set as output
-  pinMode(butPin, INPUT); //For button input
-  analogWrite(ledPin, 0); //Turn off LED while connecting
+  digitalWrite(ledPin, 0); //Turn off LED while connecting
   
   // Start Serial port monitoring
   Serial.begin(115200);
+  
   // Connect to WiFi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -63,51 +58,22 @@ void setup()
   // Print the IP address
   Serial.println(WiFi.localIP());
 
+  //Open the UDP monitoring port
   Udp.begin(localPort);
   Serial.print("Udp server started at port: ");
   Serial.println(localPort);
 
   //Register on the network with the server after verifying connect
-  fadeBrightness(0); //Fade down if the initil pin charge was high
-  fadeBrightness(70);
-  fadeBrightness(0);
-  ledSetPoint=0; // input a setpoint for fading
-  delay(2000); //Clearance to ensure registration
+  delay(2000); //Time clearance to ensure registration
   sendUdpValue("REG",sensorID1,String(deviceDescription)); //Register LED on server
+  digitalWrite(ledPin, 1); //Turn off LED while connecting
+  delay(100); //A flash of light to confirm that the lamp is ready to take commands
+  digitalWrite(ledPin, 0); //Turn off LED while connecting
+  ledSetPoint=0; // input a setpoint for fading as we enter the loop
 }
 
 void loop()
 {
-  //Check button state
-  if (millis() % butLoopTime == 0) {
-    butState=digitalRead(butPin); //0 is depressed, 1 is released
-    if ((lastButState==1 && butState==0) && (ledSetPoint != ledPinState)) {
-      Serial.println("Fade interupted");
-      butPushTime=millis();
-      sendUdpValue("LOG",sensorID2,"hold");
-    }
-    else if (lastButState==1 && butState==0) {
-      Serial.println("Button depressed");
-      butPushTime=millis();
-      sendUdpValue("LOG",sensorID2,"toggle");
-    }
-    else if (lastButState==0 && butState==1) {
-      Serial.println("Button released");
-      //sendUdpValue("LOG",sensorID2,String((millis()-butPushTime)/1000)); //Send button press to server
-    }
-    if (millis()-butPushTime == 5000 && ledPinState == 0 && butState==0) {
-      sendUdpValue("LOG",sensorID2,"all off");
-      fadeBrightness(10);
-      fadeBrightness(0);
-      butPushTime=butPushTime-1; //To prevent multiple re-runs of this step in the same millisecond
-    }
-    if (millis()-butPushTime == 1800 && ledPinState != 0 && butState==0) {
-      sendUdpValue("LOG",sensorID2,"100");
-      butPushTime=butPushTime-1; //To prevent multiple re-runs of this step in the same millisecond
-    }
-    lastButState=butState;
-  }
-
   //Code for receiving UDP messages
   int noBytes = Udp.parsePacket();
   if ( noBytes ) {
@@ -125,19 +91,19 @@ void loop()
       data = data + char(packetBuffer[i - 1]);
     } // end for
     Serial.println("Data reads: " + data);
-    devID=data.substring(0,6);
+    devID=data.substring(0,6); //Break down the message in to it's parts
     message=data.substring(7);
-    data="";
+    data=""; //reset the data variable
     Serial.println("DevID reads after transform: " + devID);
     Serial.println("Message reads after transform: " + message);
   } // end if
 
   //Conditionals for switching based on LED signal
-  if (devID==sensorID1) {
+  if (devID==sensorID1) { //Only do this set of commands if there is a message for the right device
     devID=""; //Reset so only to trigger once    
     if (message.startsWith("slow ")) { //Enables slow fading
       fadeSpeed=slowFade;
-      message=message.substring(5);
+      message=message.substring(5); //Cutting 'slow' from the message
     }
     else {
       fadeSpeed=defaultFade;
@@ -199,18 +165,6 @@ void loop()
   }
 }
 
-void fadeBrightness(int setPoint) { //input integer between 0 and 100
-  while (ledPinState < setPoint) {
-    ledPinState = ledPinState + 1;
-    analogWrite(ledPin, PWMTable[ledPinState]);
-    delay(8);
-  }
-  while (ledPinState > setPoint) {
-    ledPinState = ledPinState - 1;
-    analogWrite(ledPin, PWMTable[ledPinState]);
-    delay(8);
-  }
-}
 
 void sendUdpValue(String type, String sensorID, String value) {
   //Print GPIO state in serial
@@ -224,17 +178,6 @@ void sendUdpValue(String type, String sensorID, String value) {
   Udp.print(sensorID);
   Udp.write(",");
   Udp.print(value); //This is the value to be sent
-  Udp.endPacket();
-}
-
-void returnSignalToSender() {  
-  Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-  Udp.write("Answer from ");
-  Udp.print(sensorID1);
-  Udp.write(" with IP of ");
-  Udp.print(WiFi.localIP());
-  Udp.write(": The LED state is now set to ");
-  Udp.println(ledPinState);
   Udp.endPacket();
 }
 
